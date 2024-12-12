4f46d38ec274890ae1e1ac36311ddedf908f00c8
Nick Galvez <nicholas.galvez@gmail.com>
2024-12-12 15:18:32 -0800

    Add test data

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 /**
  * Nick Galvez
  * 210 final
+ * Using a struct, code a coffee booth queue simulation that uses a linked list as its core data structure. 
+ * The linked list's struct should be a node that contains the customer's name and drink order. 
+ * Create data arrays with names and drink orders from which to randomly create customer data.
  */
 #include <iostream>
 #include <fstream>
@@ -9,7 +12,12 @@
 
 using namespace std;
 
-const int DATA_SIZE = 10;
+const int DATA_SIZE = 10, ROUNDS = 10;
+
+string namesData[DATA_SIZE] = {
+    "Phoebe", "Joey", "Monica", "Rachel", "Ross", "Chandler", "Gunther", "Janice", "Paul Rudd", "Susan"
+}, drinkData[DATA_SIZE] = { "Espresso", "Latte", "Cappuccino", "Mocha", "Americano", "Macchiato", "Flat White", "Soy Latte", "Irish Coffee", "Turkish Coffee"
+};
 
 struct Customer
 {
