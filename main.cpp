768fe3a9dd5be480f6579453ced3647734dfe54f
Nick Galvez <nicholas.galvez@gmail.com>
2024-12-12 16:26:17 -0800

    autocommit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /**
  * Nick Galvez
  * 210 final
- * Using a struct, code a coffee booth queue simulation that uses a linked list as its core data structure. 
- * The linked list's struct should be a node that contains the customer's name and drink order. 
+ * Using a struct, code a coffee booth queue simulation that uses a linked list as its core data structure.
+ * The linked list's struct should be a node that contains the customer's name and drink order.
  * Create data arrays with names and drink orders from which to randomly create customer data.
  */
 #include <iostream>
@@ -14,10 +14,10 @@ using namespace std;
 
 const int DATA_SIZE = 10, ROUNDS = 10;
 
-string namesData[DATA_SIZE] = {
-    "Phoebe", "Joey", "Monica", "Rachel", "Ross", "Chandler", "Gunther", "Janice", "Paul Rudd", "Susan"
-}, drinkData[DATA_SIZE] = { "Espresso", "Latte", "Cappuccino", "Mocha", "Americano", "Macchiato", "Flat White", "Soy Latte", "Irish Coffee", "Turkish Coffee"
-};
+string namesData[DATA_SIZE] = {"Phoebe", "Joey", "Monica", "Rachel", "Ross", "Chandler", "Gunther", "Janice", "Paul Rudd", "Susan"},
+       drinkData[DATA_SIZE] = {"Espresso", "Latte", "Cappuccino", "Mocha", "Americano", "Macchiato", "Flat White", "Soy Latte", "Irish Coffee", "Turkish Coffee"
+    
+    };
 
 struct Customer
 {
@@ -26,11 +26,14 @@ struct Customer
     Customer *next;
 };
 
-class Queue {
-    private:
+class Queue
+{
+private:
     Customer *head;
+
 public:
-    void addCustomer() {
+    void addCustomer()
+    {
         Customer *temp = new Customer();
         temp->name = namesData[rand() % DATA_SIZE];
         temp->order = drinkData[rand() % DATA_SIZE];
@@ -38,72 +41,83 @@ public:
         head = temp;
         cout << temp->name << " has joined. They usually want a " << temp->order << endl;
     }
-    void addToEnd() {
+    void addToEnd()
+    {
         Customer *temp = new Customer();
         temp->name = namesData[rand() % DATA_SIZE];
         temp->order = drinkData[rand() % DATA_SIZE];
         cout << temp->name << " has joined. They usually want a " << temp->order << endl;
 
         temp->next = NULL;
-        if (head == NULL) {
+        if (head == NULL)
+        {
             head = temp;
             return;
         }
         Customer *last = head;
-        while (last->next != NULL) {
+        while (last->next != NULL)
+        {
             last = last->next;
         }
         last->next = temp;
     }
-    void removeCustomer(){
-        if(head == NULL){
+    void removeCustomer()
+    {
+        if (head == NULL)
+        {
             cout << "No customers in line" << endl;
             return;
         }
-        if (!head->next) {
-            cout << head->name << " has been served." << endl;   
-            delete head;   
+        if (!head->next)
+        {
+            cout << head->name << " gghas been served." << endl;
+            delete head;
             head = NULL;
             return;
         }
-    Customer *temp = head;
-    // We have to get the last item, which is the first item...
-    while (temp->next->next != NULL) {
-        temp = temp->next;
-    }
+        Customer *temp = head;
+        // We have to get the last item, which is the first item...
+        while (temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
 
-    cout << temp->next->name << " has been served." << endl;   
-    delete temp->next;
-    temp->next = NULL;
+        cout << temp->next->name << " has ffbeen served." << endl;
+        delete temp->next;
+        temp->next = NULL;
     }
-    void serve(){
-        if(head == NULL){
+    void serve()
+    {
+        if (head == NULL)
+        {
             cout << "No customers in line" << endl;
             return;
         }
-    Customer *temp = head;
-    cout << head->name << " has been served." << endl; 
-    head = head->next;
-    delete temp;
-
+        Customer *temp = head;
+        cout << head->name << " has been served." << endl;
+        head = head->next;
+        delete temp;
     }
-   
-    void display(){
+
+    void display()
+    {
         Customer *temp = head;
         int cound = 1;
-        while(temp != NULL){
+        while (temp != NULL)
+        {
             cout << cound << ": " << temp->name << ": " << temp->order << endl;
             temp = temp->next;
             cound++;
         }
     }
-    void process () {
+    void process()
+    {
         // remove a customer
-        removeCustomer();
-        
+        serve();
         // if 50% chance, add a customer
-        if (rand() % 2 == 0) {
-            addCustomer();
+        if (rand() % 2 == 0)
+        {
+            addToEnd();
         }
         display();
     }
@@ -113,21 +127,19 @@ int main()
 {
     Queue coffeeShop = Queue();
 
-    coffeeShop.addCustomer();
     coffeeShop.addToEnd();
-    // coffeeShop.addCustomer();
-    
+    coffeeShop.addToEnd();
+    coffeeShop.addToEnd();
+
     coffeeShop.display();
     cout << endl;
-    coffeeShop.removeCustomer();
-    coffeeShop.display();
 
-    for (int i = 0; i < ROUNDS; i++) {
+    for (int i = 0; i < ROUNDS; i++)
+    {
         cout << "Round " << i + 1 << endl;
         coffeeShop.process();
         cout << endl;
     }
-
 
     return 0;
 }
